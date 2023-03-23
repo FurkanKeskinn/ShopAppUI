6db8eb2851bacac18674b3c88ccd53ec6c3db4e9
Furkan Keskin <ksknmhmt3131@gmail.com>
2023-03-23 16:04:56 +0300

    Initial Commit

---
+++ ShopApp UI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ShopApp UI
+//
+//  Created by Furkan on 23.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
---
+++ ShopApp UI/ShopApp_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp_UIApp.swift
+//  ShopApp UI
+//
+//  Created by Furkan on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
